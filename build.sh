ad27742d4f4c3d4e496b72936485b3f1d32315a3
mikeeq <miotk.mikolaj@gmail.com>
2019-10-10 13:22:00 +0100

    Fedora Linux v5.3.5

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 FEDORA_KERNEL_BRANCH_NAME=f30
-FEDORA_KERNEL_COMMIT_HASH=206ff7fd1400a2c1764aeac4895dbce5d382077a      # Linux v5.2.11 - https://src.fedoraproject.org/rpms/kernel/commits/f30
+FEDORA_KERNEL_COMMIT_HASH=bb6f824ef6d6471ed1ba20779cdbd252a5936fc6      # Linux v5.3.5 - https://src.fedoraproject.org/rpms/kernel/commits/f30
 
 ### Debug commands
 echo "FEDORA_KERNEL_BRANCH_NAME=$FEDORA_KERNEL_BRANCH_NAME"
