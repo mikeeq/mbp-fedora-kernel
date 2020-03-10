b9c86d33e520621436ee9f5bd03e17d0fa766ac2
mikeeq <miotk.mikolaj@gmail.com>
2020-03-10 12:53:41 +0100

    Change branch to master

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -eu -o pipefail
 ## Update fedora docker image tag, because kernel build is using `uname -r` when defining package version variable
 FEDORA_KERNEL_GIT_URL=https://src.fedoraproject.org/rpms/kernel.git
 FEDORA_KERNEL_VERSION=5.6-rc5
-FEDORA_KERNEL_BRANCH_NAME=f31
+# FEDORA_KERNEL_BRANCH_NAME=f31
+FEDORA_KERNEL_BRANCH_NAME=master
 FEDORA_KERNEL_COMMIT_HASH=33fb0f27577c1cb87828d3aa16de25bb27cfead5      # https://src.fedoraproject.org/rpms/kernel/commits/f31
 
 ### Debug commands
