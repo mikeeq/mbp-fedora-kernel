545aa2a75d67ed991db90524f56391b315544f53
mikeeq <miotk.mikolaj@gmail.com>
2021-05-13 00:16:16 +0200

    Initial commit v5.11-F34

--- build.sh
+++ build.sh
@@ -3,80 +3,66 @@
 set -eu -o pipefail
 
 ## Update fedora docker image tag, because kernel build is using `uname -r` when defining package version variable
-FEDORA_KERNEL_GIT_URL=https://src.fedoraproject.org/rpms/kernel.git
-FEDORA_KERNEL_VERSION=5.7.12
-FEDORA_KERNEL_BRANCH_NAME=f32
-FEDORA_KERNEL_COMMIT_HASH=1427a1b472eaa6f9131d0d122fbea8b2383b9edb      # https://src.fedoraproject.org/rpms/kernel/commits/f32
+RPMBUILD_PATH=/root/rpmbuild
+FEDORA_KERNEL_VERSION=5.10.23-200.fc33      # https://bodhi.fedoraproject.org/updates/?search=&packages=kernel&releases=F33
+REPO_PWD=$(pwd)
 
 ### Debug commands
 echo "FEDORA_KERNEL_VERSION=$FEDORA_KERNEL_VERSION"
-echo "FEDORA_KERNEL_BRANCH_NAME=$FEDORA_KERNEL_BRANCH_NAME"
-echo "FEDORA_KERNEL_COMMIT_HASH=$FEDORA_KERNEL_COMMIT_HASH"
+
 pwd
-ls
 echo "CPU threads: $(nproc --all)"
 grep 'model name' /proc/cpuinfo | uniq
-# git clone --depth 1 --single-branch --branch v5.1.19 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 
 ### Dependencies
 dnf install -y fedpkg fedora-packager rpmdevtools ncurses-devel pesign git libkcapi libkcapi-devel libkcapi-static libkcapi-tools zip curl
 
-### Clone Fedora Kernel git repo
-git clone --single-branch --branch $FEDORA_KERNEL_BRANCH_NAME ${FEDORA_KERNEL_GIT_URL}
-cd kernel
-## Cleanup
-rm -rfv ./*.rpm
-rm -rf ~/rpmbuild/*
-git reset --hard $FEDORA_KERNEL_BRANCH_NAME
-git checkout $FEDORA_KERNEL_BRANCH_NAME
-git branch -d fedora_patch_src &>/dev/null || true
-fedpkg clean
-## Change branch
-git checkout $FEDORA_KERNEL_COMMIT_HASH
-git reset --hard $FEDORA_KERNEL_COMMIT_HASH
-git checkout -b fedora_patch_src
-dnf -y builddep kernel.spec
+## Set home build directory
+rpmdev-setuptree
 
-### Fixes for kernel.spec
-# sed -i "s/Patch509/Patch516/g" kernel.spec
+## Install the kernel source and finish installing dependencies
+cd ${RPMBUILD_PATH}/SOURCES
+koji download-build --arch=src kernel-${FEDORA_KERNEL_VERSION}
+rpm -Uvh kernel-${FEDORA_KERNEL_VERSION}.src.rpm
+
+cd ${RPMBUILD_PATH}/SPECS
+dnf -y builddep kernel.spec
 
 ### Create patch file with custom drivers
 echo >&2 "===]> Info: Creating patch file... ";
-FEDORA_KERNEL_VERSION=${FEDORA_KERNEL_VERSION} ../patch_driver.sh
+FEDORA_KERNEL_VERSION=${FEDORA_KERNEL_VERSION} ${REPO_PWD}/patch_driver.sh
 
 ### Apply patches
-if [ ! -f scripts/newpatch.sh ]; then
-  cp -rf ../fedora/newpatch.sh scripts/newpatch.sh
-fi
 echo >&2 "===]> Info: Applying patches... ";
-[ ! -d ../patches ] && { echo 'Patches directory not found!'; exit 1; }
+mkdir -p ${REPO_PWD}/patches
 while IFS= read -r file
 do
   echo "adding $file"
-  scripts/newpatch.sh "$file"
-done < <(find ../patches -type f -name "*.patch" | sort)
+  ${REPO_PWD}/patch_kernel.sh "$file"
+done < <(find ${REPO_PWD}/patches -type f -name "*.patch" | sort)
 
 ### Change buildid to mbp
 echo >&2 "===]> Info: Setting kernel name... ";
-sed -i 's/%define buildid.*/%define buildid .mbp/' ./kernel.spec
-
-### Build src rpm
-echo >&2 "===]> Info: Bulding src.rpm ... ";
-fedpkg --release $FEDORA_KERNEL_BRANCH_NAME srpm
+sed -i 's/%define buildid.*/%define buildid .mbp/' ${RPMBUILD_PATH}/SPECS/kernel.spec
 
 ### Build non-debug rpms
 echo >&2 "===]> Info: Bulding kernel ... ";
-./scripts/fast-build.sh x86_64 "$(find . -type f -name "*.src.rpm")"
+# cd ${RPMBUILD_PATH}/SOURCES
+# rpmbuild --target x86_64 --without debug --without debuginfo --without perf --without tools --rebuild kernel-${FEDORA_KERNEL_VERSION}.src.rpm
+cd ${RPMBUILD_PATH}/SPECS
+# rpmbuild -bb --without debug --target=x86_64 kernel.spec
+rpmbuild -bb --without debug --without debuginfo --without perf --without tools --target=x86_64 kernel.spec
 rpmbuild_exitcode=$?
 
 ### Copy artifacts to shared volume
 echo >&2 "===]> Info: Copying rpms and calculating SHA256 ... ";
-cp -rfv /root/rpmbuild/RPMS/x86_64/*.rpm /tmp/artifacts/
-sha256sum /root/rpmbuild/RPMS/x86_64/*.rpm > /tmp/artifacts/sha256
+cp -rfv ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm /tmp/artifacts/
+sha256sum ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm > /tmp/artifacts/sha256
 
 ### Add patches to artifacts
-cd ..
+cd ${REPO_PWD}
 zip -r patches.zip patches/
 cp -rfv patches.zip /tmp/artifacts/
+du -h /tmp/artifacts/
 
 exit $rpmbuild_exitcode
