f3d83da612a69bad0fe43c358745faa5ac595e44
mikeeq <miotk.mikolaj@gmail.com>
2021-06-23 12:40:21 +0200

    Remove grub2 packages

--- yum-repo/provision.sh
+++ yum-repo/provision.sh
@@ -6,7 +6,7 @@ cd /var/repo
 
 # https://koji.fedoraproject.org/koji/packageinfo?packageID=6684
 GRUB_MAIN_VERSION=2.04
-GRUB_SUFFIX_VERSION=35
+GRUB_SUFFIX_VERSION=31
 GRUB_FEDORA_VERSION=fc34
 
 ### Download previous versions of kernel
@@ -18,30 +18,32 @@ GRUB_FEDORA_VERSION=fc34
 # && rm -rfv fedora-mbp-repo.herokuapp.com
 
 ### Download older version of grub 2.04
-grub_pkgs_x86_64=(
-  grub2-efi-ia32
-  grub2-efi-ia32-cdboot
-  grub2-efi-x64
-  grub2-efi-x64-cdboot
-  grub2-pc
-  grub2-tools
-  grub2-tools-efi
-  grub2-tools-extra
-  grub2-tools-minimal
-)
+# grub_pkgs_x86_64=(
+#   grub2-efi-ia32
+#   grub2-efi-ia32-cdboot
+#   grub2-efi-x64
+#   grub2-efi-x64-cdboot
+#   grub2-pc
+#   grub2-tools
+#   grub2-tools-efi
+#   grub2-tools-extra
+#   grub2-tools-minimal
+# )
 
-grub_pkgs_noarch=(
-  grub2-common
-  grub2-pc-modules
-)
+# grub_pkgs_noarch=(
+#   grub2-common
+#   grub2-pc-modules
+#   grub2-efi-x64-modules
+#   grub2-efi-ia32-modules
+# )
 
-for i in "${grub_pkgs_x86_64[@]}"; do
-  curl -Ls https://kojipkgs.fedoraproject.org//packages/grub2/"${GRUB_MAIN_VERSION}"/"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}"/x86_64/"${i}"-"${GRUB_MAIN_VERSION}"-"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}".x86_64.rpm -O
-done
+# for i in "${grub_pkgs_x86_64[@]}"; do
+#   curl -Ls https://kojipkgs.fedoraproject.org//packages/grub2/"${GRUB_MAIN_VERSION}"/"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}"/x86_64/"${i}"-"${GRUB_MAIN_VERSION}"-"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}".x86_64.rpm -O
+# done
 
-for i in "${grub_pkgs_noarch[@]}"; do
-  curl -Ls https://kojipkgs.fedoraproject.org//packages/grub2/"${GRUB_MAIN_VERSION}"/"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}"/noarch/"${i}"-"${GRUB_MAIN_VERSION}"-"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}".noarch.rpm -O
-done
+# for i in "${grub_pkgs_noarch[@]}"; do
+#   curl -Ls https://kojipkgs.fedoraproject.org//packages/grub2/"${GRUB_MAIN_VERSION}"/"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}"/noarch/"${i}"-"${GRUB_MAIN_VERSION}"-"${GRUB_SUFFIX_VERSION}"."${GRUB_FEDORA_VERSION}".noarch.rpm -O
+# done
 
 curl -Ls https://kojipkgs.fedoraproject.org//packages/shim/15/8/x86_64/shim-ia32-15-8.x86_64.rpm -O
 curl -Ls https://kojipkgs.fedoraproject.org//packages/shim/15/8/x86_64/shim-x64-15-8.x86_64.rpm -O
