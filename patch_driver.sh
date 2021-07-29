3505011046fcab81281be15b3d8640acfe91b14d
mikeeq <miotk.mikolaj@gmail.com>
2021-07-29 12:41:36 +0200

    Check jamlam patches v2

--- patch_driver.sh
+++ patch_driver.sh
@@ -6,6 +6,7 @@ set -eu -o pipefail
 
 ### Apple T2 drivers commit hashes
 APPLE_SMC_DRIVER_GIT_URL=https://github.com/jamlam/mbp-16.1-linux-wifi
+APPLE_SMC_REPO_NAME=mbp-16.1-linux-wifi
 APPLE_SMC_DRIVER_BRANCH_NAME=main
 APPLE_SMC_DRIVER_COMMIT_HASH=46e4665e286862d76d29701a334515a77734c58f
 
@@ -21,12 +22,12 @@ mkdir -p "${PATCHES_DIR}"
 
 ### AppleSMC and BT aunali fixes
 git clone --single-branch --branch ${APPLE_SMC_DRIVER_BRANCH_NAME} ${APPLE_SMC_DRIVER_GIT_URL}
-cd linux-mbp-arch || exit
+cd "${APPLE_SMC_REPO_NAME}" || exit
 git checkout ${APPLE_SMC_DRIVER_COMMIT_HASH}
 cd ..
 while IFS= read -r file; do
   echo "adding ${file}"
   cp -rfv "${file}" "${PATCHES_DIR}"/"${file##*/}"
-done < <(find mbp-16.1-linux-wifi -type f -name "*patch" | sort)
+done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*patch" | sort)
 
 rm -rf "${TMP_DIR}"
