8ceba2ee416d9997cc603930d5a4b5f39f075528
mikeeq <miotk.mikolaj@gmail.com>
2021-12-01 09:26:03 +0100

    Fedora Linux v5.15.5

--- patch_driver.sh
+++ patch_driver.sh
@@ -8,7 +8,7 @@ set -eu -o pipefail
 APPLE_SMC_DRIVER_GIT_URL=https://github.com/jamlam/mbp-16.1-linux-wifi
 APPLE_SMC_REPO_NAME=mbp-16.1-linux-wifi
 APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=5ae467cc48f9d865d17039bc7b5646574fcb208f
+APPLE_SMC_DRIVER_COMMIT_HASH=911efd340fd956e927f480d9b565c29f4f947d5b
 
 # TMP_DIR=~/temp_dir
 TMP_DIR=/tmp/temp_dir
