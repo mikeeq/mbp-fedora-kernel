8ca1da82c2c279a84ede236de42ef4d799767167
mikeeq <miotk.mikolaj@gmail.com>
2022-05-16 15:24:42 +0200

    test build v3

--- patch_driver.sh
+++ patch_driver.sh
@@ -28,6 +28,36 @@ cd ..
 while IFS= read -r file; do
   echo "adding ${file}"
   cp -rfv "${file}" "${PATCHES_DIR}"/"${file##*/}"
-done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*patch" | grep -v 1001 | grep -v 1002 | grep -v 1003 | sort)
+done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*patch" | grep -v 1001 | grep -v 1002 | grep -v 1004 | sort)
 
 rm -rf "${TMP_DIR}"
+
+# Patch2: 1004-add-modalias-to-apple-bce.patch
+# + case "$patch" in
+# + patch -p1 -F2 -s
+# The text leading up to this was:
+# --------------------------
+# |From 153b587ed53135eaf244144f6f8bdd5a0fe6b69e Mon Sep 17 00:00:00 2001
+# |From: Redecorating <69827514+Redecorating@users.noreply.github.com>
+# |Date: Fri, 24 Dec 2021 18:12:25 +1100
+# |Subject: [PATCH 1/1] add modalias to apple-bce
+# |
+# |---
+# | drivers/staging/apple-bce/apple_bce.c     |  1 +
+# | 1 files changed, 1 insertions(+), 0 deletions(-)
+# |
+# |diff --git a/drivers/staging/apple-bce/apple_bce.c b/drivers/staging/apple-bce/apple_bce.c
+# |index a6a656f..8cfbd3f 100644
+# |--- a/drivers/staging/apple-bce/apple_bce.c
+# |+++ b/drivers/staging/apple-bce/apple_bce.c
+# --------------------------
+# File to patch:
+# Skip this patch? [y]
+# 1 out of 1 hunk ignored
+# error: Bad exit status from /var/tmp/rpm-tmp.83tGk6 (%prep)
+
+
+# RPM build errors:
+#     Macro expanded in comment on line 188: %define with_kabichk   %{?_without_kabichk:   0} %{?!_without_kabichk:   1}
+
+#     Bad exit status from /var/tmp/rpm-tmp.83tGk6 (%prep)
