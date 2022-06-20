b971ce6ec0cb1a91c093bcb74d1cc4ca984aef40
zeroday0619 <zeroday0619_dev@outlook.com>
2022-06-20 14:34:05 +0900

    Fedora Kernel v5.18.5 F36

--- update_kernel_mbp.sh
+++ update_kernel_mbp.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 ### Apple T2 drivers commit hashes
 KERNEL_PATCH_PATH=/tmp/kernel_patch
 
-UPDATE_SCRIPT_BRANCH=${UPDATE_SCRIPT_BRANCH:-v5.17-f36}
+UPDATE_SCRIPT_BRANCH=${UPDATE_SCRIPT_BRANCH:-v5.18-f36}
 MBP_FEDORA_BRANCH=f36
 BCE_DRIVER_GIT_URL=https://github.com/t2linux/apple-bce-drv
 BCE_DRIVER_BRANCH_NAME=aur
