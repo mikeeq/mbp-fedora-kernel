9c20bca05319e101b30f6af62876de39a6464c33
Mikołaj Miotk <miotk.mikolaj@gmail.com>
2022-06-27 20:42:25 +0200

    Fedora Kernel v5.18.6 F36 (#60)
    * Fedora Kernel v5.18.5 F36

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Drivers:
 ```bash
 ### First run or if you want to update your copy of update_kernel_mbp script
 sudo -i
-curl -L https://raw.githubusercontent.com/mikeeq/mbp-fedora-kernel/v5.17-f36/update_kernel_mbp.sh -o /usr/bin/update_kernel_mbp
+curl -L https://raw.githubusercontent.com/mikeeq/mbp-fedora-kernel/v5.18-f36/update_kernel_mbp.sh -o /usr/bin/update_kernel_mbp
 chmod +x /usr/bin/update_kernel_mbp
 update_kernel_mbp
 
@@ -25,19 +25,19 @@ update_kernel_mbp
 
 ### Update to specific version of kernel
 sudo -i
-KERNEL_VERSION="5.17.6-f36" update_kernel_mbp
+KERNEL_VERSION="5.18.6-f36" update_kernel_mbp
 
 ### Update to specific version of kernel using specific version of update script
 #### Usually not needed, because scripts are shared between branches, but you can use it to update your update_kernel_mbp script
 ##### If the script fails, try to rerun it - it's due to self-upgrading feature of this script
 sudo -i
-KERNEL_VERSION="5.17.6-f36" UPDATE_SCRIPT_BRANCH="v5.17-f36" update_kernel_mbp
+KERNEL_VERSION="5.18.6-f36" UPDATE_SCRIPT_BRANCH="v5.18-f36" update_kernel_mbp
 ```
 
 ## CI status
 
-Drone kernel build status:
-[![Build Status](https://cloud.drone.io/api/badges/mikeeq/mbp-fedora-kernel/status.svg)](https://cloud.drone.io/mikeeq/mbp-fedora-kernel)
+GitHub Actions kernel build status:
+[![Build Status](https://github.com/mikeeq/mbp-fedora-kernel/actions/workflows/build-kernel.yml/badge.svg)](https://github.com/mikeeq/mbp-fedora-kernel/actions/workflows/build-kernel.yml)
 
 Github Actions kernel publish status - <http://fedora-mbp-repo.herokuapp.com/> :
 [![Publish Status](https://github.com/mikeeq/mbp-fedora-kernel/actions/workflows/yum-repo.yml/badge.svg)](https://github.com/mikeeq/mbp-fedora-kernel/actions/workflows/yum-repo.yml)
--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 ## Update fedora docker image tag, because kernel build is using `uname -r` when defining package version variable
 RPMBUILD_PATH=/root/rpmbuild
 MBP_VERSION=mbp
-FEDORA_KERNEL_VERSION=5.17.6-300.fc36      # https://bodhi.fedoraproject.org/updates/?search=&packages=kernel&releases=F36
+FEDORA_KERNEL_VERSION=5.18.6-200.fc36      # https://bodhi.fedoraproject.org/updates/?search=&packages=kernel&releases=F36
 REPO_PWD=$(pwd)
 
 ### Debug commands
@@ -30,37 +30,45 @@ cd ${RPMBUILD_PATH}/SPECS
 dnf -y builddep kernel.spec
 
 ### Create patch file with custom drivers
-echo >&2 "===]> Info: Creating patch file... ";
+echo >&2 "===]> Info: Creating patch file...";
 FEDORA_KERNEL_VERSION=${FEDORA_KERNEL_VERSION} "${REPO_PWD}"/patch_driver.sh
 
 ### Apply patches
-echo >&2 "===]> Info: Applying patches... ";
+echo >&2 "===]> Info: Applying patches...";
 mkdir -p "${REPO_PWD}"/patches
 while IFS= read -r file
 do
-  echo "adding $file"
+  echo >&2 "===]> Info: Applying patch: $file"
   "${REPO_PWD}"/patch_kernel.sh "$file"
 done < <(find "${REPO_PWD}"/patches -type f -name "*.patch" | sort)
 
+echo >&2 "===]> Info: Applying kconfig changes... ";
+echo "CONFIG_APPLE_BCE=m" >> "${RPMBUILD_PATH}/SOURCES/kernel-local"
+echo "CONFIG_APPLE_IBRIDGE=m" >> "${RPMBUILD_PATH}/SOURCES/kernel-local"
+
 ### Change buildid to mbp
-echo >&2 "===]> Info: Setting kernel name... ";
+echo >&2 "===]> Info: Setting kernel name...";
 sed -i "s/# define buildid.*/%define buildid .${MBP_VERSION}/" "${RPMBUILD_PATH}"/SPECS/kernel.spec
 
 ### Build non-debug rpms
-echo >&2 "===]> Info: Bulding kernel ... ";
+echo >&2 "===]> Info: Bulding kernel ...";
 cd "${RPMBUILD_PATH}"/SPECS
-rpmbuild -bb --without debug --without debuginfo --without perf --without tools --target=x86_64 kernel.spec
+rpmbuild -bb --with baseonly --without debug --without debuginfo --target=x86_64 kernel.spec
 rpmbuild_exitcode=$?
 
 ### Copy artifacts to shared volume
-echo >&2 "===]> Info: Copying rpms and calculating SHA256 ... ";
-cp -rfv ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm /tmp/artifacts/
-sha256sum ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm > /tmp/artifacts/sha256
+echo >&2 "===]> Info: Copying rpms and calculating SHA256 ...";
+cd "${REPO_PWD}"
+mkdir -p ./output_zip
+cp -rfv ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm ./output_zip/
+sha256sum ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm > ./output_zip/sha256
 
 ### Add patches to artifacts
-cd "${REPO_PWD}"
 zip -r patches.zip patches/
-cp -rfv patches.zip /tmp/artifacts/
-du -h /tmp/artifacts/
+cp -rfv patches.zip ./output_zip/
+echo
+du -sh ./output_zip
+echo
+du -sh ./output_zip/*.rpm
 
 exit $rpmbuild_exitcode
