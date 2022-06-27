f9721043abd9e716d07258486b6c698b20dd5a43
mikeeq <miotk.mikolaj@gmail.com>
2022-06-27 18:05:05 +0200

    test build with 100x patches - f36

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -2,8 +2,8 @@
 
 set -eu -o pipefail
 
-DOCKER_IMAGE=fedora:35
-# DOCKER_IMAGE=fedora_build:35
+DOCKER_IMAGE=fedora:36
+# DOCKER_IMAGE=fedora_build:36
 RPMBUILD_HOST_PATH=~/rpmbuild
 
 mkdir -p ${RPMBUILD_HOST_PATH}
