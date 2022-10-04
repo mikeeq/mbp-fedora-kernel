47fb2e7b31057783092e5881ff7e5716eff93c56
mikeeq <miotk.mikolaj@gmail.com>
2022-10-04 16:27:08 +0200

    new kernel v3

--- build.sh
+++ build.sh
@@ -43,9 +43,11 @@ do
 done < <(find "${REPO_PWD}"/patches -type f -name "*.patch" | sort)
 
 echo >&2 "===]> Info: Applying kconfig changes... ";
-echo "CONFIG_APPLE_BCE=m" >> "${RPMBUILD_PATH}/SOURCES/kernel-local"
-echo "CONFIG_APPLE_IBRIDGE=m" >> "${RPMBUILD_PATH}/SOURCES/kernel-local"
-echo "CONFIG_BT_HCIBCM4377=m" >> "${RPMBUILD_PATH}/SOURCES/kernel-local"
+{
+  echo "CONFIG_APPLE_BCE=m"
+  echo "CONFIG_APPLE_IBRIDGE=m"
+  echo "CONFIG_BT_HCIBCM4377=m"
+} >> "${RPMBUILD_PATH}/SOURCES/kernel-local"
 
 ### Change buildid to mbp
 echo >&2 "===]> Info: Setting kernel name...";
