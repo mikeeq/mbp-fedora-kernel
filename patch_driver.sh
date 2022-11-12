0c89e758ad0b5238003b5b45e2e38da6aa957693
mikeeq <miotk.mikolaj@gmail.com>
2022-11-12 13:22:05 +0100

    Fedora Linux v6.0.8

--- patch_driver.sh
+++ patch_driver.sh
@@ -4,16 +4,16 @@ set -eu -o pipefail
 
 # set -x
 
-### Apple T2 drivers commit hashes
-# APPLE_SMC_DRIVER_GIT_URL=https://github.com/Redecorating/mbp-16.1-linux-wifi
-# APPLE_SMC_REPO_NAME=mbp-16.1-linux-wifi
-# APPLE_SMC_DRIVER_BRANCH_NAME=main
-# APPLE_SMC_DRIVER_COMMIT_HASH=0f18a8ee0e2eb7893222e3d0f433f75ce689aa91
+APPLE_BCE_REPOSITORY=https://github.com/kekrby/apple-bce.git
+APPLE_IBRIDGE_REPOSITORY=https://github.com/Redecorating/apple-ib-drv.git
 
-APPLE_SMC_DRIVER_GIT_URL=https://github.com/AdityaGarg8/linux-t2-patches
+RPMBUILD_PATH=${RPMBUILD_PATH:-/root/rpmbuild}
+KERNEL_PATH=${RPMBUILD_PATH}/SOURCES
+
+APPLE_SMC_DRIVER_GIT_URL=https://github.com/t2linux/linux-t2-patches
 APPLE_SMC_REPO_NAME=linux-t2-patches
 APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=9dcb1e2d259c8bcdb3242000a552e21f6fabf8cd
+APPLE_SMC_DRIVER_COMMIT_HASH=42eefd1c0331c20efedc5674508c32d52575f723
 
 # TMP_DIR=~/temp_dir
 TMP_DIR=/tmp/temp_dir
@@ -26,6 +26,9 @@ cd "${TMP_DIR}" || exit
 rm -rf "${PATCHES_DIR}"
 mkdir -p "${PATCHES_DIR}"
 
+git clone --depth 1 "${APPLE_BCE_REPOSITORY}" "${KERNEL_PATH}/drivers/staging/apple-bce"
+git clone --depth 1 "${APPLE_IBRIDGE_REPOSITORY}" "${KERNEL_PATH}/drivers/staging/apple-ibridge"
+
 ### AppleSMC and BT aunali fixes
 git clone --single-branch --branch ${APPLE_SMC_DRIVER_BRANCH_NAME} ${APPLE_SMC_DRIVER_GIT_URL}
 cd "${APPLE_SMC_REPO_NAME}" || exit
@@ -34,6 +37,6 @@ cd ..
 while IFS= read -r file; do
   echo "adding ${file}"
   cp -rfv "${file}" "${PATCHES_DIR}"/"${file##*/}"
-done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | sort)
+done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | sort | grep -v -e 1001 -e 1002)
 
 rm -rf "${TMP_DIR}"
