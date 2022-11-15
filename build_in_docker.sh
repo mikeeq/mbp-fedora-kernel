77f91e8a5dc56fc3501405f725b3636164b85508
mikeeq <miotk.mikolaj@gmail.com>
2022-11-15 19:32:40 +0100

    fix adding custom drivers v2

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -12,23 +12,23 @@ mkdir -p ${RPMBUILD_HOST_PATH}
 
 RPM_SIGNING_KEY=${RPM_SIGNING_KEY:-$(gpg --export-secret-keys -a 'mbp-fedora' | base64)}
 
-# docker pull "${DOCKER_IMAGE}"
-# docker run \
-#   -t \
-#   --rm \
-#   -e RPM_SIGNING_KEY="$RPM_SIGNING_KEY" \
-#   -v "$(pwd)":/repo \
-#   -v ${RPMBUILD_HOST_PATH}:/root/rpmbuild \
-#   -w /repo \
-#   "${DOCKER_IMAGE}" \
-#   /bin/bash -c './build.sh'
-
+docker pull "${DOCKER_IMAGE}"
 docker run \
-  -it \
+  -t \
   --rm \
   -e RPM_SIGNING_KEY="$RPM_SIGNING_KEY" \
   -v "$(pwd)":/repo \
   -v ${RPMBUILD_HOST_PATH}:/root/rpmbuild \
   -w /repo \
-  ${DOCKER_IMAGE} \
-  /bin/bash
+  "${DOCKER_IMAGE}" \
+  /bin/bash -c './build.sh'
+
+# docker run \
+#   -it \
+#   --rm \
+#   -e RPM_SIGNING_KEY="$RPM_SIGNING_KEY" \
+#   -v "$(pwd)":/repo \
+#   -v ${RPMBUILD_HOST_PATH}:/root/rpmbuild \
+#   -w /repo \
+#   ${DOCKER_IMAGE} \
+#   /bin/bash
