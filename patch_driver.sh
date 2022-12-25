b46ab38abd03a3744fade86dba883619575f0990
mikeeq <miotk.mikolaj@gmail.com>
2022-12-26 00:28:37 +0100

    skip 7001 patch

--- patch_driver.sh
+++ patch_driver.sh
@@ -54,7 +54,7 @@ cd ..
 while IFS= read -r file; do
   echo "adding ${file}"
   cp -rfv "${file}" "${PATCHES_DIR}"/"${file##*/}"
-done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | sort)
+done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | sort | grep -v 7001)
 
 rm -rf "${TMP_DIR}"
 # rm -rf "${TMP_REPOS_DIR}"
