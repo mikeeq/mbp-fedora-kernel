6610b43eb4e50439b60e30f16473a21404ce80b1
mikeeq <miotk.mikolaj@gmail.com>
2023-01-13 08:19:16 +0100

    Try new src pkg

--- build.sh
+++ build.sh
@@ -7,13 +7,11 @@ set -eu -o pipefail
 ## Update fedora docker image tag, because kernel build is using `uname -r` when defining package version variable
 RPMBUILD_PATH=/root/rpmbuild
 MBP_VERSION=mbp
-# FEDORA_KERNEL_VERSION=6.1.5-65.fc38                     # https://bodhi.fedoraproject.org/updates/?search=&packages=kernel&releases=F37
-FEDORA_KERNEL_VANILLA_VERSION=6.1.5-250.vanilla.1.fc37    # https://fedorapeople.org/groups/repos/thl/kernel-vanilla-fedora/fedora-37/SRPMS/
+FEDORA_KERNEL_VERSION=6.1.5-200.fc37      # https://bodhi.fedoraproject.org/updates/?search=&packages=kernel&releases=F37
 REPO_PWD=$(pwd)
 
 ### Debug commands
-# echo "FEDORA_KERNEL_VERSION=$FEDORA_KERNEL_VERSION"
-echo "FEDORA_KERNEL_VANILLA_VERSION=$FEDORA_KERNEL_VANILLA_VERSION"
+echo "FEDORA_KERNEL_VERSION=$FEDORA_KERNEL_VERSION"
 
 pwd
 echo "CPU threads: $(nproc --all)"
@@ -28,19 +26,15 @@ rpmdev-setuptree
 
 ## Install the kernel source and finish installing dependencies
 cd ${RPMBUILD_PATH}/SOURCES
-# koji download-build --arch=src kernel-${FEDORA_KERNEL_VERSION}
-# rpm -Uvh kernel-${FEDORA_KERNEL_VERSION}.src.rpm
-
-curl -L "https://fedorapeople.org/groups/repos/thl/kernel-vanilla-fedora/fedora-37/SRPMS/kernel-${FEDORA_KERNEL_VANILLA_VERSION}.src.rpm" -O
-rpm -Uvh "kernel-${FEDORA_KERNEL_VANILLA_VERSION}.src.rpm"
+koji download-build --arch=src kernel-${FEDORA_KERNEL_VERSION}
+rpm -Uvh kernel-${FEDORA_KERNEL_VERSION}.src.rpm
 
 cd ${RPMBUILD_PATH}/SPECS
 dnf -y builddep kernel.spec
 
 ### Create patch file with custom drivers
 echo >&2 "===]> Info: Creating patch file...";
-# FEDORA_KERNEL_VERSION=${FEDORA_KERNEL_VERSION} "${REPO_PWD}"/kernel_patches.sh
-"${REPO_PWD}"/kernel_patches.sh
+FEDORA_KERNEL_VERSION=${FEDORA_KERNEL_VERSION} "${REPO_PWD}"/kernel_patches.sh
 
 ### Apply patches
 echo >&2 "===]> Info: Applying patches...";
