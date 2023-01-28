d862ccdf18fae44030f13090d050535ed7a31ed9
mikeeq <miotk.mikolaj@gmail.com>
2023-01-28 11:41:11 +0100

    Fedora Linux v6.1.8

--- kernel_patches.sh
+++ kernel_patches.sh
@@ -10,7 +10,7 @@ set -eu -o pipefail
 APPLE_SMC_DRIVER_GIT_URL=https://github.com/t2linux/linux-t2-patches
 APPLE_SMC_REPO_NAME=linux-t2-patches
 APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=6bdb333610f43b59a2245d299a302300c992c764
+APPLE_SMC_DRIVER_COMMIT_HASH=ad1f804d54bc5c263695c6140d4602c5787004d1
 
 TMP_DIR=/tmp/tmp_dir
 REPO_PWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
