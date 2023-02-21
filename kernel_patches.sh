81156d51f4e2fdc400a0f623c4ab90fc7481a2d7
mikeeq <miotk.mikolaj@gmail.com>
2023-02-21 17:26:06 +0100

    Revert "test 3009 patch"
    This reverts commit 5bdf8f2e60e73f8108728788465e6812f3890894.

--- kernel_patches.sh
+++ kernel_patches.sh
@@ -10,7 +10,7 @@ set -eu -o pipefail
 APPLE_SMC_DRIVER_GIT_URL=https://github.com/t2linux/linux-t2-patches
 APPLE_SMC_REPO_NAME=linux-t2-patches
 APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=98e76c4c5bcdb5ce5a3752f68c83e0bcea953654
+APPLE_SMC_DRIVER_COMMIT_HASH=5d7fec1df97018a29147917531b248992b5368e9
 
 TMP_DIR=/tmp/tmp_dir
 REPO_PWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
@@ -32,9 +32,6 @@ while IFS= read -r file; do
   cp -rfv "${file}" "${PATCHES_DIR}"/"${file##*/}"
 done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | sort)
 
-curl -Ls "https://raw.githubusercontent.com/t2linux/linux-t2-patches/5d7fec1df97018a29147917531b248992b5368e9/8001-asahilinux-wifi-patchset.patch" -o "${PATCHES_DIR}/8001-asahilinux-wifi-patchset.patch"
-
-# curl -Ls "https://raw.githubusercontent.com/t2linux/linux-t2-patches/98e76c4c5bcdb5ce5a3752f68c83e0bcea953654/3008-applesmc-make-applesmc_remove-void.patch" -o "${PATCHES_DIR}/3008-applesmc-make-applesmc_remove-void.patch"
-# curl -Ls "https://raw.githubusercontent.com/t2linux/linux-t2-patches/98e76c4c5bcdb5ce5a3752f68c83e0bcea953654/3009-applesmc-battery-charge-limiter.patch" -o "${PATCHES_DIR}/3009-applesmc-battery-charge-limiter.patch"
+curl -Ls "https://raw.githubusercontent.com/t2linux/linux-t2-patches/98e76c4c5bcdb5ce5a3752f68c83e0bcea953654/3009-applesmc-battery-charge-limiter.patch" -o "${PATCHES_DIR}/3009-applesmc-battery-charge-limiter.patch"
 
 rm -rf "${TMP_DIR}"
