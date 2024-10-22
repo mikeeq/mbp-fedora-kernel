6d360bd0cc54345d8dbdb46ee931366fba9657b4
mikeeq <miotk.mikolaj@gmail.com>
2024-10-22 17:40:39 +0200

    test build with 10* patches

--- kernel_patches.sh
+++ kernel_patches.sh
@@ -30,6 +30,6 @@ cd ..
 while IFS= read -r file; do
   echo "adding ${file}"
   cp -rfv "${file}" "${PATCHES_DIR}"/"${file##*/}"
-done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | sort)
+done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | grep -i -v 10 | sort)
 
 rm -rf "${TMP_DIR}"
